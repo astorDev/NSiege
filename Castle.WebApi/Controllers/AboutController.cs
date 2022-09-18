da9480a2d0754dda36ecc6888a441f84d43acd09
tarasov_es <pavluchenk0@yandex.ru>
2022-09-18 13:23:16 +0400

    cleanup aboutcontroller

--- Castle.WebApi/Controllers/AboutController.cs
+++ Castle.WebApi/Controllers/AboutController.cs
@@ -2,7 +2,6 @@
 public class AboutController
 {
     public IHostEnvironment Environment { get; }
-    
     public AboutController(IHostEnvironment environment) {
         this.Environment = environment;
     }
@@ -10,10 +9,6 @@ public class AboutController
     [HttpGet]
     public async Task<About> GetAbout()
     {
-        var delaySecs = new Random().NextDouble() * 3.5;
-        await Task.Delay(TimeSpan.FromSeconds(delaySecs));
-        if (DateTime.Now.Millisecond % 5 == 0) throw new InvalidOperationException("Cannot use about now");
-
         return new(
             Description : "Castle webapi",
             Version : this.GetType().Assembly.GetName().Version!.ToString(),
